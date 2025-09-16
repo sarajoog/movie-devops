ec5265ebf68703d14bbe5cec68a7c2155cab0032
Ajuuke <asarajoog@gmail.com>
2025-09-16 09:58:38 -0400

    Initial commit

---
+++ src/main/java/com/ajuuke/movie_devops/MovieDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.ajuuke.movie_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajuuke/movie_devops/MovieDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajuuke.movie_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
